3b54990e1c1472bf9c1c2a9092fd14d4361708b5
Bastin Robins J <bastinrobins@gmail.com>
2018-12-09 13:16:01 +0530

    Added: docs directory added

---
+++ sparxDB/query.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+class Query:
+    """ `Query Class for database operations """
+	def __init__(self):
+		pass
+
+
+	def insert(self):
+		"""
+		
+		"""
+		pass
+
+	def update(self):
+		pass
+
+
+	def fetch(self):
+		pass
+
+
+	def find(self):
+		pass
+
+
+	def delete(self):
+		pass
+
+
+
